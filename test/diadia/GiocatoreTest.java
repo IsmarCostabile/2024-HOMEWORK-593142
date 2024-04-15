9c2ade9723c6a0327645777eb6fadec2a7a58276
Ismar Costabile <41170553+IsmarCostabile@users.noreply.github.com>
2024-04-15 16:26:57 +0200

    Add files via upload

---
+++ test/diadia/GiocatoreTest.java
@@ -0,0 +1,16 @@
+package diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GiocatoreTest {
+
+	// Non ancora necessario, implementa solo metodi ad altri metodi
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
